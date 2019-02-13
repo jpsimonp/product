dcadde35b8cdd73977ca41cec03342394a26bc4c
jpsimon <jpsimon@indra.es>
2019-02-13 18:19:12 +0100

    Evolutivo caso  6  + 2:   clase CommonData evoluciona  tanto en proyecto como en producto pero solo evoluciona en destino la clase de proyecto.

--- HelloWorldRest/src/main/java/com/example/rest/data/CommonData.java
+++ HelloWorldRest/src/main/java/com/example/rest/data/CommonData.java
@@ -10,9 +10,15 @@ package com.example.rest.data;
 public class CommonData {
 	
 	private String evolution1;
+	private int evolution2;
 	
 	public CommonData(String e) {
 		this.evolution1 = e;
+	}
+	
+	public CommonData(String e, int e2) {
+		this.evolution1 = e;
+		this.evolution2 = e2;
 	}
 
 	public String getEvolution1() {
@@ -21,5 +27,13 @@ public class CommonData {
 
 	public void setEvolution1(String evolution1) {
 		this.evolution1 = evolution1;
+	}
+
+	public int getEvolution2() {
+		return evolution2;
+	}
+
+	public void setEvolution2(int evolution2) {
+		this.evolution2 = evolution2;
 	}
 }
