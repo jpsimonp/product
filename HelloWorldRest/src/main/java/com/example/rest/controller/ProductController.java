6ce0180a8e0e546a59921a6b7c47ddc91780440c
jpsimon <jpsimon@indra.es>
2019-02-18 12:44:42 +0100

    Segundo cambio impl02

--- HelloWorldRest/src/main/java/com/example/rest/controller/ProductController.java
+++ HelloWorldRest/src/main/java/com/example/rest/controller/ProductController.java
@@ -52,12 +52,12 @@ public class ProductController {
 	@ResponseBody
 	public CommonMatchData evolution5() {
 		return new CommonMatchData("evolution5");
-	}*/
+	}
 	
 	@GetMapping(value = "/product/evolution6", produces = "application/json")
 	@ResponseBody
 	public CommonMatchData evolution6() {
 		return new CommonMatchData("evolution6");
-	}
+	}*/
 
 }
